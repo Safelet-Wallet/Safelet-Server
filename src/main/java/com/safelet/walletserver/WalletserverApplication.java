8269fe3d01730b085ae44674596333f9b1f0964c
Andrés Sánchez <andresmarnez@gmail.com>
2023-01-25 15:38:34 +0100

    Initial Commit

---
+++ src/main/java/com/safelet/walletserver/WalletserverApplication.java
@@ -0,0 +1,13 @@
+package com.safelet.walletserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safelet/walletserver/WalletserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safelet.walletserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
